3f2104abe4e4dddad84c05389308b95c5a028d95
Jason <raspberrypy660@gmail.com>
2023-05-20 22:50:20 -0400

    test

---
+++ node_modules/http-mitm-proxy/lib/middleware/gunzip.ts
@@ -0,0 +1,19 @@
+import zlib from "zlib";
+import type { IContext } from "../../types";
+
+export default {
+  onResponse(ctx: IContext, callback: Function) {
+    const serverToProxyResponse = ctx.serverToProxyResponse!;
+    if (
+      serverToProxyResponse.headers["content-encoding"]?.toLowerCase() == "gzip"
+    ) {
+      delete serverToProxyResponse.headers["content-encoding"];
+      ctx.addResponseFilter(zlib.createGunzip());
+    }
+    return callback();
+  },
+  onRequest(ctx: IContext, callback: Function) {
+    ctx.proxyToServerRequestOptions!.headers["accept-encoding"] = "gzip";
+    return callback();
+  },
+};
---
+++ node_modules/http-mitm-proxy/lib/middleware/wildcard.ts
@@ -0,0 +1,22 @@
+/**
+ * group1: subdomain
+ * group2: domain.ext
+ * exclude short domains (length < 4) to avoid catching double extensions (ex: net.au, co.uk, ...)
+ */
+import { ErrorCallback, IProxy } from "../../types";
+
+const HOSTNAME_REGEX = /^(.+)(\.[^.]{4,}(\.[^.]{1,3})*\.[^.]+)$/;
+
+export default {
+  onCertificateRequired(hostname: string, callback: ErrorCallback) {
+    let rootHost = hostname;
+    if (HOSTNAME_REGEX.test(hostname)) {
+      rootHost = hostname.replace(/^[^.]+\./, "");
+    }
+    return callback(null, {
+      keyFile: (<IProxy>this).sslCaDir + "/keys/_." + rootHost + ".key",
+      certFile: (<IProxy>this).sslCaDir + "/certs/_." + rootHost + ".pem",
+      hosts: ["*." + rootHost, rootHost],
+    });
+  },
+};
